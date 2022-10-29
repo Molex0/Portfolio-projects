438725b2288783c05a7f25a12b075def7999a0e1
fanti <gavrylyuk987@gmail.com>
2022-10-29 20:17:46 +0300

    check links

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -14,7 +14,7 @@ const Projects = () => {
               <h1 className='projects__title'>Projects</h1>
               <h4 className='projects__text'>There are some projects that I've made during mastering Frontend Development</h4>
               <div className='projects-grid'>
-                  <Card id={1} image = {p1} title='Responsive React App' link=''/>
+                  <Card id={1} image = {p1} title='Responsive React App' link='https://dev.to/github/how-to-use-github-pages-to-host-your-website-even-with-multiple-repos-27k2'/>
                   <Card id={2} image = {p2} title='Meme Generator'link=''/>
                   <Card id={3} image = {p3} title='Responsive App' link='https://molex0.github.io/ProjectCSS-host/'/>
                   <Card id={4} image = {p4} title='React Tenzies Game'link=''/>
